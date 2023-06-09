1e86a858f85ccf7fd38eb23002d5daaeaf8a82cd
Wahyu Ramadan <wramadan1203@gmail.com>
2023-06-09 17:01:24 +0700

    description function

--- src/helpers/queryExecute.ts
+++ src/helpers/queryExecute.ts
@@ -1,6 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { pool } from '../configs/db.config';
 
+/**
+ * Function to execude query
+ * @param query string
+ * @param params any
+ * @returns array
+ */
 const executeQuery = async (query: string, params?: any) => pool.query(query, params)
   .then(([rows, fields]) => rows)
   .catch((error) => {
